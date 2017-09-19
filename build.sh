d156321dd2740c74922d648cf0bafc4ce405e5cf
Sebastian Fyda (tawnos) <sebastian.fyda@getstreetteam.com>
2017-09-19 16:20:00 +0200

    updted dockers

---
+++ build.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+set -e;
+pushd $(dirname "$0") > /dev/null;
+
+source config
+HUB="quay.io"
+NAMESPACE="bulldocker"
+REPO=${1};
+
+if [[ -z "${REPO}" ]]; then
+    echo "Usage $0 repo"
+    exit 1
+fi
+
+TAG=${2};
+if [[ -z "${TAG}" ]]; then
+    TAG="latest";
+fi;
+
+image=${HUB}/${NAMESPACE}/${REPO}
+cd ${REPO}
+docker build -f Dockerfile \
+	-t ${image}:${TAG} \
+	-t ${image}:latest \
+	.;
+
+docker login -u=${DOCKER_USER} -p=${DOCKER_PASS} ${HUB}
+docker push ${image}:${TAG}
+popd > /dev/null;
+exit 0;
